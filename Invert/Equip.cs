e3edf460b03ecf1b2344766a572df88f32dfb368
gr691_shrn <gr691_shrn@RDP-NODE2>
2021-06-08 11:15:44 +0700

    Проверка

--- Invert/Equip.cs
+++ Invert/Equip.cs
@@ -168,6 +168,5 @@ namespace Invert
             }
             return true;
         }
-
     }
 }
--- Invert/Invert_Cabinet.xaml.cs
+++ Invert/Invert_Cabinet.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
 using Tulpep.NotificationWindow;
 using Color = System.Drawing.Color;
 
@@ -21,52 +22,115 @@ namespace Invert
     /// </summary>
     public partial class Invert_Cabinet : Window
     {
-        Equip equip = new Equip();
         public gr691_invert db;
+        Equip equip = new Equip();
         public Invert_Cabinet()
         {
             InitializeComponent();
             db = new gr691_invert();
+            List<Equipment> us = db.Database.SqlQuery<Equipment>("Select*from Equipment Where indef = 'Монитор'").ToList();
+            //Monitor_Count.Content = Convert.ToString(us.Count);
+            us = db.Database.SqlQuery<Equipment>("Select*from Equipment Where indef = 'Стул'").ToList();
+
+            us = db.Database.SqlQuery<Equipment>("Select*from Equipment Where indef = 'Стол'").ToList();
+            Table_Count.Text = Convert.ToString(us.Count);
+            us = db.Database.SqlQuery<Equipment>("Select*from Equipment Where indef = 'Компьютер'").ToList();
+            List<Equipment> ps = db.Equipment.Where(db => db.indef == "Стул").ToList();
+            Chair_Count.Text = Convert.ToString(ps.Count);
+            Computer_Case_Count.Text = Convert.ToString(us.Count);
+
+            Table_Cabinet.ItemsSource = db.Equipment.ToList();
         }
+
         //ВКЛАДКА КАБИНЕТОВ
-        private void Cb_Cabinet_1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        public static string selectedCabinet { get; set; }
+        public void Cb_Cabinet_1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            db = new gr691_invert();
+
             if (Cb_Cabinet_1.SelectedIndex == 0)
             {
-                //показать полный список
-                //скорее всего, нужно тупо сложить кол-во оборудования во всех кабинетах
+                Monitor_Count.Text = db.Equipment.Where(w => w.indef == "Монитор").Count().ToString();
+                Chair_Count.Text = db.Equipment.Where(db => db.indef == "Стул").Count().ToString();
+                Table_Count.Text = db.Equipment.Where(db => db.indef == "Стол").Count().ToString();
+                Computer_Case_Count.Text = db.Equipment.Where(db => db.indef == "Компьютер").Count().ToString();
+                selectedCabinet = "0";
+                Table_Cabinet.ItemsSource = db.Equipment.ToList();
             }
             else if (Cb_Cabinet_1.SelectedIndex == 1)
             {
-                //показать список оборудования кабинета 1
+                Monitor_Count.Text = db.Equipment.Where(db => db.indef == "Монитор" && db.cabinet == "1").Count().ToString();
+                Chair_Count.Text = db.Equipment.Where(db => db.indef == "Стул" && db.cabinet == "1").Count().ToString();
+                Table_Count.Text = db.Equipment.Where(db => db.indef == "Стол" && db.cabinet == "1").Count().ToString();
+                Computer_Case_Count.Text = db.Equipment.Where(db => db.indef == "Компьютер" && db.cabinet == "1").Count().ToString();
+                selectedCabinet = "1";
+                Table_Cabinet.ItemsSource = db.Equipment.Where(d => d.cabinet == "1").ToList();
             }
             else if (Cb_Cabinet_1.SelectedIndex == 2)
             {
-                //показать список оборудования кабинета 2
+                Monitor_Count.Text = db.Equipment.Where(db => db.indef == "Монитор" && db.cabinet == "2").Count().ToString();
+                Chair_Count.Text = db.Equipment.Where(db => db.indef == "Стул" && db.cabinet == "2").Count().ToString();
+                Table_Count.Text = db.Equipment.Where(db => db.indef == "Стол" && db.cabinet == "2").Count().ToString();
+                Computer_Case_Count.Text = db.Equipment.Where(db => db.indef == "Компьютер" && db.cabinet == "2").Count().ToString();
+                selectedCabinet = "2";
+                Table_Cabinet.ItemsSource = db.Equipment.Where(d => d.cabinet == "2").ToList();
             }
             else if (Cb_Cabinet_1.SelectedIndex == 3)
             {
-                //показать список оборудования кабинета 3
+                Monitor_Count.Text = db.Equipment.Where(db => db.indef == "Монитор" && db.cabinet == "3").Count().ToString();
+                Chair_Count.Text = db.Equipment.Where(db => db.indef == "Стул" && db.cabinet == "3").Count().ToString();
+                Table_Count.Text = db.Equipment.Where(db => db.indef == "Стол" && db.cabinet == "3").Count().ToString();
+                Computer_Case_Count.Text = db.Equipment.Where(db => db.indef == "Компьютер" && db.cabinet == "3").Count().ToString();
+                selectedCabinet = "3";
+                Table_Cabinet.ItemsSource = db.Equipment.Where(d => d.cabinet == "3").ToList();
             }
         }
         private void Monitor_Click(object sender, RoutedEventArgs e)
-        {        
-            //MessageBox.Show(db.User.id)
+        {
+            if (selectedCabinet != "0")
+            {
+                Table_Cabinet.ItemsSource = db.Equipment.Where(d => d.cabinet == selectedCabinet && d.indef == "Монитор").ToList();
+            }
+            else
+            {
+                Table_Cabinet.ItemsSource = db.Equipment.Where(d => d.indef == "Монитор").ToList();
+            }
         }
 
         private void Table_Click(object sender, RoutedEventArgs e)
         {
-
+            if (selectedCabinet != "0")
+            {
+                Table_Cabinet.ItemsSource = db.Equipment.Where(d => d.cabinet == selectedCabinet && d.indef == "Стол").ToList();
+            }
+            else
+            {
+                Table_Cabinet.ItemsSource = db.Equipment.Where(d => d.indef == "Стол").ToList();
+            }
         }
 
         private void Chair_Click(object sender, RoutedEventArgs e)
         {
-
+            if (selectedCabinet != "0")
+            {
+                Table_Cabinet.ItemsSource = db.Equipment.Where(d => d.cabinet == selectedCabinet && d.indef == "Стул").ToList();
+            }
+            else
+            {
+                Table_Cabinet.ItemsSource = db.Equipment.Where(d => d.indef == "Стул").ToList();
+            }
         }
 
         private void Computer_Case_Click(object sender, RoutedEventArgs e)
         {
-
+            if (selectedCabinet != "0")
+            {
+                Table_Cabinet.ItemsSource = db.Equipment.Where(d => d.cabinet == selectedCabinet && d.indef == "Компьютер").ToList();
+            }
+            else
+            {
+                Table_Cabinet.ItemsSource = db.Equipment.Where(d => d.indef == "Компьютер").ToList();
+            }
         }
         //ВКЛАДКА РЕДАКТИРОВАНИЯ ТАБЛИЦЫ
         private void Insert(object sender, RoutedEventArgs e)
