08535f1f9920cdc69923ae4b7a0fee0207afae6f
gr691_shrn <gr691_shrn@RDP-NODE2>
2021-06-08 10:25:08 +0700

    Revert "Сделана вкладка "Общее редактирование""
    This reverts commit 4d9efa0627787ac0d26f2ea494f6c83831e14feb.

--- Invert/Invert_Cabinet.xaml.cs
+++ Invert/Invert_Cabinet.xaml.cs
@@ -11,9 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using Tulpep.NotificationWindow;
-using Color = System.Drawing.Color;
-
+using MaterialDesignThemes.Wpf;
 namespace Invert
 {
     /// <summary>
@@ -21,8 +19,7 @@ namespace Invert
     /// </summary>
     public partial class Invert_Cabinet : Window
     {
-        Equip equip = new Equip();
-        public gr691_invert db;
+        gr691_invert db;
         public Invert_Cabinet()
         {
             InitializeComponent();
@@ -71,61 +68,27 @@ namespace Invert
         //ВКЛАДКА РЕДАКТИРОВАНИЯ ТАБЛИЦЫ
         private void Insert(object sender, RoutedEventArgs e)
         {
-            if (equip.Add(Cb_Equipment.SelectedIndex.ToString(), Cb_Cabinet_2.Text) == true)
-            {
-                Cb_Cabinet_2.SelectedIndex = -1;
-                Cb_Equipment.SelectedIndex = -1;
-            }
-            Table_Editing.ItemsSource = db.Equipment.ToList();
+            
         }
 
         private void Delete(object sender, RoutedEventArgs e)
         {
-            Equipment equipment = Table_Editing.SelectedItem as Equipment;
-            PopupNotifier popup = new PopupNotifier();
-            popup.TitleText = "Ошибка";
-            popup.BodyColor = Color.LightGray;
-            if (Table_Editing.SelectedItem == null)
-            {
-                popup.ContentText = "Вы не выбрали кабинет";
-                popup.Popup();
-                return;
-            }
-            db.Equipment.Where(i => i.id == equipment.id).FirstOrDefault();
-            equip.Delete(equipment != null ? equipment.id.ToString() : "0");
-            Table_Editing.ItemsSource = db.Equipment.ToList();
+
         }
 
         private void Update(object sender, RoutedEventArgs e)
         {
-            Equipment equipment = Table_Editing.SelectedItem as Equipment;
-            PopupNotifier popup = new PopupNotifier();
-            popup.TitleText = "Ошибка";
-            popup.BodyColor = Color.LightGray;
-            if (Table_Editing.SelectedItem == null)
-            {
-                popup.ContentText = "Вы не выбрали кабинет";
-                popup.Popup();
-                return;
-            }
-            db.Equipment.Where(i => i.id == equipment.id).FirstOrDefault();
-            if (equip.Update(equipment != null ? equipment.id.ToString() : "0", Cb_Equipment.SelectedIndex.ToString(), Cb_Cabinet_2.Text) == true)
-            {
-                gr691_invert db = new gr691_invert();
-                Cb_Cabinet_2.SelectedIndex = -1;
-                Cb_Equipment.SelectedIndex = -1;
-                Table_Editing.ItemsSource = db.Equipment.ToList();
-            }
-        }
-        private void Exit(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
+
         }
         //ЗАГРУЗКА ДАННЫХ В ТАБЛИЦАХ
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             db = new gr691_invert();
-            Table_Editing.ItemsSource = db.Equipment.ToList();
+            //вывод таблицы
+        }
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
